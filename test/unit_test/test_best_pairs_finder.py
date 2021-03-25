9a959369da4758d986448d58e02fb7c108dad09a
Krauhausen, I <i.krauhausen@tue.nl>
2021-03-25 17:22:15 +0100

    Initial commit

---
+++ test/unit_test/test_best_pairs_finder.py
@@ -0,0 +1,30 @@
+from unittest import TestCase, skip
+
+from best_pairs_finder import BestPairsFinder
+
+
+class TestBestPairsFinder(TestCase):
+
+    def test_nothing(self):
+        self.assertTrue(True)
+
+    def test_zero_particles(self):
+        pairs_finder = BestPairsFinder()
+        particle_positions = list()
+        best_pairs = pairs_finder.find_best_pairs(particle_positions=particle_positions)
+        self.assertTrue([[]] == best_pairs)
+
+    def test_two_particles_in_two_dimensions(self):
+        pairs_finder = BestPairsFinder()
+        particle_positions = [[0., -1.], [1., 2.]]
+        best_pairs = pairs_finder.find_best_pairs(particle_positions=particle_positions)
+        self.assertCountEqual([[[0., -1.], [1., 2.]]], best_pairs)
+
+    def test_four_particles_in_two_dimensions(self):
+        pairs_finder = BestPairsFinder()
+        particle_positions = [[0., 0.], [1., 1.], [9., 9], [10., 10.]]
+        best_pairs = pairs_finder.find_best_pairs(particle_positions=particle_positions)
+        self.assertCountEqual([
+                    [[0., 0.], [1., 1.]],
+                    [[9., 9.], [10., 10]]
+                        ], best_pairs)
