8cf49c1894c809aa171a895d63b9b0049b117d22
Ronaldo Rodrigues Pela <ronaldorpela@gmail.com>
2021-04-03 13:25:34 +0200

    created function to sort the solution w.r.t. the 1st coordinate

--- src/best_pairs_finder_non_brute_force.py
+++ src/best_pairs_finder_non_brute_force.py
@@ -1,6 +1,6 @@
 import numpy
 
-class BestPairsFinder:
+class BestPairsFinderNonBruteForce:
 
     def find_best_pairs(self, particle_positions):
         """
@@ -9,20 +9,19 @@ class BestPairsFinder:
         should return [[[0, 0], [1, 1]], [[20, 20],[21, 21]]]  (or equivalent shufflings)
         """
         if len(particle_positions) == 4:
-            return [[particle_positions[0], particle_positions[1]],[particle_positions[2], particle_positions[3]]]
-        return [particle_positions]
-
-    # def check_combinations(self, particle_positions):
-    #     distance_sums = list()
-    #     distance_sums.append(self.calc_dist_of_two_particles(particle_positions[0], particle_positions[1]) +
-    #                          self.calc_dist_of_two_particles(particle_positions[2], particle_positions[3]))
-    #     distance_sums.append(self.calc_dist_of_two_particles(particle_positions[1], particle_positions[2]) +
-    #                          self.calc_dist_of_two_particles(particle_positions[0], particle_positions[3]))
-    #     distance_sums.append(self.calc_dist_of_two_particles(particle_positions[0], particle_positions[2]) +
-    #                          self.calc_dist_of_two_particles(particle_positions[1], particle_positions[3]))
-    #
-    #     ind = numpy.argmin(distance_sums)
-    #     return best_solution
+            return [
+            [particle_positions[0], particle_positions[1]],
+            [particle_positions[2], particle_positions[3]]
+            ]
+        return self.sort_solution( [particle_positions] )
 
     def calc_dist_of_two_particles(self, particle1, particle2):
-        return numpy.linalg.norm(numpy.asarray(particle1) - numpy.asarray(particle2))
+        return numpy.linalg.norm(
+            numpy.asarray(particle1) - numpy.asarray(particle2)
+            )
+
+    def sort_solution( self, PairsList ):
+        if len( PairsList[0] ) > 0 :
+            for pairs in PairsList:
+                pairs.sort()
+        return PairsList
