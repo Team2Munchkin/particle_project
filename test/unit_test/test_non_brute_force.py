74d488d75bbe793e46e10b392fd3dcf8988cc98d
Ronaldo Rodrigues Pela <ronaldorpela@gmail.com>
2021-04-03 13:22:10 +0200

    added more tests for 2 particles (to also test the order of the solution)

--- test/unit_test/test_non_brute_force.py
+++ test/unit_test/test_non_brute_force.py
@@ -1,16 +1,19 @@
 from unittest import TestCase, skip
-from src.best_pairs_finder import BestPairsFinder
+from src.best_pairs_finder_non_brute_force import BestPairsFinderNonBruteForce
 import pytest
 
 @pytest.mark.parametrize('particle_list, expected_best_pairs',
      [
-         (list(), [[]]),
+         ([], [[]]),
          ([[0., -1.], [1., 2.]], [[[0., -1.], [1., 2.]]]),
+         ([[1., 2.], [0., -1.]], [[[0., -1.], [1., 2.]]]),
+         ([[-1, 0.], [0., -1.]], [[[-1., 0.], [0., -1.]]]),
+         ([[0., 1.], [0., -1.]] ,[[[0,-1],[0,1]]] ),
          ([[0., 0.], [1., 1.], [9., 9], [10., 10.]],
           [[[0., 0.], [1., 1.]], [[9., 9.], [10., 10]]])
      ])
 def test_best_positions(particle_list, expected_best_pairs):
-    pairs_finder = BestPairsFinder()
+    pairs_finder = BestPairsFinderNonBruteForce()
     assert pairs_finder.find_best_pairs(particle_positions=particle_list) == expected_best_pairs
 
 @pytest.mark.parametrize('two_particles_positions, expected_distance',
@@ -22,33 +25,11 @@ def test_best_positions(particle_list, expected_best_pairs):
          ([ [2.,0.,2.], [0.,3.,0.] ], 17**0.5 )
      ])
 def test_distance(two_particles_positions, expected_distance):
-    pairs_finder = BestPairsFinder()
+    pairs_finder = BestPairsFinderNonBruteForce()
     assert pairs_finder.calc_dist_of_two_particles(
         particle1=two_particles_positions[0], particle2=two_particles_positions[1]
         ) == expected_distance
 
 class TestBestPairsFinder(TestCase):
-
     def test_nothing(self):
         self.assertTrue(True)
-    #
-    # def test_zero_particles(self):
-    #     pairs_finder = BestPairsFinder()
-    #     particle_positions = list()
-    #     best_pairs = pairs_finder.find_best_pairs(particle_positions=particle_positions)
-    #     self.assertTrue([[]] == best_pairs)
-    #
-    # def test_two_particles_in_two_dimensions(self):
-    #     pairs_finder = BestPairsFinder()
-    #     particle_positions = [[0., -1.], [1., 2.]]
-    #     best_pairs = pairs_finder.find_best_pairs(particle_positions=particle_positions)
-    #     self.assertCountEqual([[[0., -1.], [1., 2.]]], best_pairs)
-    #
-    # def test_four_particles_in_two_dimensions(self):
-    #     pairs_finder = BestPairsFinder()
-    #     particle_positions = [[0., 0.], [1., 1.], [9., 9], [10., 10.]]
-    #     best_pairs = pairs_finder.find_best_pairs(particle_positions=particle_positions)
-    #     self.assertCountEqual([
-    #                 [[0., 0.], [1., 1.]],
-    #                 [[9., 9.], [10., 10]]
-    #                     ], best_pairs)
